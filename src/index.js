ae1dae0646bfef2dab6c870c5c7a291e924d716c
Pras Pitasawad <prasrodo@gmail.com>
2021-12-26 21:42:07 +0700

    change index.js to render MainMenu

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MainMenu from './components/MainMenu';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainMenu />
   </React.StrictMode>,
   document.getElementById('root')
 );
